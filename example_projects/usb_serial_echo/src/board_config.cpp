3d1ba62945769e769f843b6e1734c79e32854e28
Matthew Lai <m@matthewlai.ca>
2018-08-29 03:34:09 +0100

    Added USB serial echo example

---
+++ example_projects/usb_serial_echo/src/board_config.cpp
@@ -0,0 +1,18 @@
+#include "ostrich.h"
+
+#include <libopencm3/stm32/rcc.h>
+
+namespace Ostrich {
+BoardConfig MakeBoardConfig() {
+  BoardConfig bc;
+
+  bc.clock_scale = rcc_3v3[RCC_CLOCK_3V3_216MHZ];
+  bc.hse_mhz = 12;
+  bc.use_hse = true;
+
+  // 1 ms.
+  bc.systick_period_clocks = 216000;
+
+  return bc;
+}
+}
---
+++ example_projects/usb_serial_echo/src/main.cpp
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the libostrich project.
+ *
+ * Copyright (C) 2018 Matthew Lai <m@matthewlai.ca>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+
+#include "ostrich.h"
+#include "usb/serial.h"
+
+using Ostrich::USBSerial;
+
+int main() {
+  USBSerial serial;
+
+  bool port_open = false;
+
+  while (true) {
+    if (!port_open && serial.PortOpen()) {
+      // Port has just been opened (DTR received).
+      serial << "Port opened" << std::endl;
+    }
+
+    port_open = serial.PortOpen();
+
+    if (serial.DataAvailable()) {
+      std::string line;
+      std::getline(serial, line);
+      serial << line << std::endl;
+    }
+  }
+}
