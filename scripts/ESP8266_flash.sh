7d5b7bdd5b53e802c5cd8d0053162d4f30ab5e47
Matthew Lai <m@matthewlai.ca>
2019-09-22 01:56:20 +0100

    Updated libopencm3

--- scripts/ESP8266_flash.sh
+++ scripts/ESP8266_flash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-esptool.py -p /dev/ttyACM0 --flash_size 2MB-c1 write_flash 0x0 boot_v1.7.bin 0x01000 at/1024+1024/user1.2048.new.5.bin 0x1fc000 esp_init_data_default_v08.bin 0xfe000 blank.bin  0x1fe000 blank.bin
+esptool.py -p /dev/ttyACM0 write_flash --flash_size 2MB-c1 0x0 boot_v1.7.bin 0x01000 at/1024+1024/user1.2048.new.5.bin 0x1fc000 esp_init_data_default_v08.bin 0xfe000 blank.bin  0x1fe000 blank.bin
